3903b94d31f2b5d7830c573c5b79761559235b57
Guilherme Ferreira <guilherme27set@gmail.com>
2016-07-11 14:42:25 -0300

    Alteração em calculadora.js valor default pagPorVisualizacao

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,12 +3,17 @@ var calculadora = function(){
     var receita_e = document.getElementById('receita_estimada').value;
     var calc_valor = document.getElementById('calc_valor').value;
     var calc_visualizacao = document.getElementById('calc_visualizacao').value;
-
-    var pagPorVisualizacao = receita_e/total_v;
-    
-    var rend_calc_valor = calc_valor / pagPorVisualizacao;
-    var rend_calc_visualizacao = pagPorVisualizacao * calc_visualizacao;
+   
     if(total_v && receita_e){
+        var pagPorVisualizacao = receita_e/total_v;
+        var rend_calc_valor = calc_valor / pagPorVisualizacao;
+        var rend_calc_visualizacao = pagPorVisualizacao * calc_visualizacao;
+        document.getElementById('res_calc_valor').innerHTML = rend_calc_valor.toFixed(2)+" Visualizações";
+        document.getElementById('res_calc_visualizacao').innerHTML = "$ "+rend_calc_visualizacao.toFixed(2)+" dólares";
+    }else{
+        var pagPorVisualizacao = 4.93/3545;
+        var rend_calc_valor = calc_valor / pagPorVisualizacao;
+        var rend_calc_visualizacao = pagPorVisualizacao * calc_visualizacao;
         document.getElementById('res_calc_valor').innerHTML = rend_calc_valor.toFixed(2)+" Visualizações";
         document.getElementById('res_calc_visualizacao').innerHTML = "$ "+rend_calc_visualizacao.toFixed(2)+" dólares";
     }
